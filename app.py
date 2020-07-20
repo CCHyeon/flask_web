dcf2902773c3ba9a621786abb0b3279dc65fff5f
CCHyeon <all03853@gmail.com>
2020-07-20 16:08:18 +0900

    navbar

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask ,render_template
 
 app = Flask(__name__)
 app.debug=True
 
-@app.route('/data')
-
+@app.route('/')
 def index():
     print("Success")
-    #return "TEST"
-    return render_template('home.html', hello="CCHyeon")
+    # return "TEST"
+    return render_template('home.html',hello="CChyeon")
 
-if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port='8080')
+@app.route('/about')
+def about():
+    print("Success")
+    # return "TEST"
+    return render_template('about.html',hello="CChyeon")
+
+@app.route('/articles')
+def articles():
+    print("Success")
+    # return "TEST"
+    return render_template('articles.html',hello="CChyeon")
+
+if __name__ =='__main__':
+    # app.run(host='0.0.0.0', port='8080')
     app.run()
-
